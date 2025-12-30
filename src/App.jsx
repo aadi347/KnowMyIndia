56f9f582f1cc0b2766769c8d276dd08fe0de3532
Aditya Kumar <aadi678a@gmail.com>
2025-12-30 09:35:43 +0530

    added analytics tool from vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import NewHero from "./components/NewHero";
 import StateInfo from "./components/StateInfo";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <div className="root">
       <div>
+        <Analytics />
         <StateInfo />
       </div>
     </div>
